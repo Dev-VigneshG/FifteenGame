2a43a7cd1ecb4edf18ec10c4ca04f086ad4d8bc6
Vignesh G <71144564+Dev-VigneshG@users.noreply.github.com>
2024-10-19 13:32:19 +0530

    Update FifteenGameRunner.java

--- FifteenGame/src/runner/FifteenGameRunner.java
+++ FifteenGame/src/runner/FifteenGameRunner.java
@@ -1,16 +1,41 @@
+/*
+ * TITLE:FIFTEEN PUZZLE
+ * AUTHOR:VIGNESH G
+ * DESCRIPTION:
+ *  The main objective of this game is to rearrange the shuffled cells into the correct order.
+ *
+ * PACKAGES:
+ * 1)game   -contains game glasses
+ * 2)runner -used to run the Game
+ * 
+ * CLASSES:
+ * 1)FifteenPuzzle(game)       -represents the game
+ * 2)Square(game)              -represent the each cell in the board
+ * 3)FifteenGameRunner(runner) -This Class runs the game 
+ * 
+ * TOOL:
+ * 1)Java SE 21
+ * 2)Eclipse IDE
+ */
 package runner;
 import java.util.Scanner;
 import game.FifteenPuzzle;
 public class FifteenGameRunner {
-      public static void main(String[] args)
-      {
-    	  Scanner scanner=new Scanner(System.in);
-    	  System.out.println("********************Welcome to 15 Puzzle Game!!!***********");
-    	  System.out.println("Enter Size of The Puzzle: (Give One Value ex:5):");
-    	  int noOfRowsColumns=scanner.nextInt();
-    	  FifteenPuzzle fifteenPuzzle=new FifteenPuzzle(noOfRowsColumns);
-    	  fifteenPuzzle.createBoard();
-          fifteenPuzzle.shuffleBoard();
-    	  fifteenPuzzle.play();
-      }
+	public static void main(String[] args)
+	{
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("********************Welcome to 15 Puzzle Game!!!***********");
+			System.out.println("Enter Size of The Puzzle: (Give One Value ex:5):");
+			//getting no of row and columns.board must be square. So only one value needed
+			int noOfRowsColumns=scanner.nextInt();
+			//Initialize the game
+			FifteenPuzzle fifteenPuzzle=new FifteenPuzzle(noOfRowsColumns);
+			//creating the board
+			fifteenPuzzle.createBoard();
+			//Shuffling the board
+			fifteenPuzzle.shuffleBoard();
+			//start the game
+			fifteenPuzzle.play();
+		}
+	}
 }
